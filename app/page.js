1365cfec461526b1f10c848a80e01d50b081aacc
800006913 <yvdantas@banestes.com.br>
2025-04-16 19:41:20 -0300

    section 92: reserved file names custom components & how to organize a nextjs project

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <Header />
+      <p>🔥 Let's get started! 🔥</p>
       <p>
         <Link href="/about">About Us</Link>
       </p>
