2fdc6e9f192776de68404ba6689e187440e61c0d
800006913 <yvdantas@banestes.com.br>
2025-04-23 21:04:14 -0300

    section 95: configuring dynamic routes & using route parameters

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,11 @@
+export default function BlogPostPage({ params }) {
+  return (
+    <main>
+      <h1>Blog Post</h1>
+      <p>Post: {params.slug}</p>
+      <p>Post: {params.slug}</p>
+      <p>Post: {params.slug}</p>
+      <p>Post: {params.slug}</p>
+    </main>
+  );
+}
